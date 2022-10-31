4adfcf646601143fb1fb40bed50b6d0911a997ca
nll004 <loninath17@gmail.com>
2022-10-31 10:17:20 -0400

    basic styling added

--- src/DogList.js
+++ src/DogList.js
@@ -4,15 +4,16 @@ import { useEffect } from "react";
 import {v4 as uuid} from "uuid";
 
 import Dog from "./Dog";
+import './DogList.css';
 
 function DogList({dogs}){
     const [dogInfo, setDogInfo] = useState(dogs);
     const { name } = useParams();
 
     useEffect(()=>{
-        for(let i = 0; i < dogs.length; i++){
-            if(name === dogs[i].name.toLowerCase()) {
-                setDogInfo([dogs[i]])
+        for(let dog of dogs){
+            if(name === dog.name.toLowerCase()) {
+                setDogInfo([dog])
             }
         }
     }, [ name ])
@@ -20,7 +21,7 @@ function DogList({dogs}){
     return (
         <>
         <h1>Dog List</h1>
-        <section className="Dog-container">
+        <section id="Dog-container">
             {dogInfo.map( dg => (
                 <Dog key={uuid()}
                      src={dg.src}
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {v4 as uuid} from "uuid";
 import { NavLink } from "react-router-dom";
+import "./NavBar.css";
 
 function NavBar({dogs}){
 
     return (
         <nav>
+            <NavLink exact to='/dogs'> All Dogs </NavLink>
             {dogs.map(dg => (
                 <NavLink to={`/dogs/${dg.name.toLowerCase()}`}
                          key={uuid()}
